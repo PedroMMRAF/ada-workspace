0ad7f30730203799b8713bfefcdf01aecc490c1d
PedroMMRAF <pmmr.fernandes@campus.fct.unl.pt>
2023-03-21 19:10:18 +0000

    extra

--- Projeto1/src/Solver.java
+++ Projeto1/src/Solver.java
@@ -11,112 +11,77 @@ public class Solver {
     private static final char TROLL_PLOT = 't';
     private static final char DRAGON_PLOT = 'd';
 
-    private static final int HARP_TIME = 4;
-    private static final int POTION_TIME = 5;
-    private static final int CLOAK_TIME = 6;
-    private static final int CROSS_WITH_ITEM_TIME = 3;
+    private static final int NO_ITEM = 0;
+    private static final int HARP = 1;
+    private static final int POTION = 2;
+    private static final int CLOAK = 3;
+    private static final int[] items = { NO_ITEM, HARP, POTION, CLOAK };
 
-    private static final int INF = 1000000;
+    private static final int CROSS_PLOT_WITHOUT_ITEM = 1;
+    private static final int CROSS_PLOT_WITH_ITEM = 3;
 
     private final String plot;
 
-    private int noItemTime;
-    private int harpTime;
-    private int potionTime;
-    private int cloakTime;
+    private int[] times;
 
     private int foundMonster;
 
     public Solver(String plot) {
         this.plot = plot;
 
-        foundMonster = noItemTime = 0;
-        harpTime = potionTime = cloakTime = INF;
+        times = new int[4];
+
+        times[NO_ITEM] = foundMonster = 0;
+        times[HARP] = times[POTION] = times[CLOAK] = Integer.MIN_VALUE;
     }
 
     public int solve() {
         for (int i = 0; i < plot.length(); i++) {
             switch (plot.charAt(i)) {
-                case EMPTY_PLOT -> crossEmptyField();
-                case HARP_PLOT -> crossHarpField();
-                case POTION_PLOT -> crossPotionField();
-                case CLOAK_PLOT -> crossCloakField();
-                case DOG_PLOT -> crossDogField();
-                case TROLL_PLOT -> crossTrollField();
-                case DRAGON_PLOT -> crossDragonField();
+                case EMPTY_PLOT -> crossEasyPlot(NO_ITEM);
+                case HARP_PLOT -> crossEasyPlot(HARP);
+                case POTION_PLOT -> crossEasyPlot(POTION);
+                case CLOAK_PLOT -> crossEasyPlot(CLOAK);
+                case DOG_PLOT -> crossPlot(HARP);
+                case TROLL_PLOT -> crossPlot(POTION);
+                case DRAGON_PLOT -> crossPlot(CLOAK);
             }
         }
         return getMinOfTimes();
     }
 
-    private void crossEmptyField() {
-        noItemTime = getMinOfTimes() + 1 + foundMonster;
+    private void crossEasyPlot(int plotItem) {
+        // We drop any item after finding a monster
+        times[NO_ITEM] = getMinOfTimes() + CROSS_PLOT_WITHOUT_ITEM + foundMonster;
 
-        harpTime += CROSS_WITH_ITEM_TIME;
-        potionTime += CROSS_WITH_ITEM_TIME;
-        cloakTime += CROSS_WITH_ITEM_TIME;
-
-        foundMonster = 0;
-    }
-
-    private void crossHarpField() {
-        noItemTime = getMinOfTimes() + 1 + foundMonster;
-        harpTime = noItemTime + 1;
-
-        potionTime += CROSS_WITH_ITEM_TIME;
-        cloakTime += CROSS_WITH_ITEM_TIME;
+        for (int item : items)
+            if (plotItem == item)
+                // Just picked up the item
+                times[item] = times[NO_ITEM] + 1;
+            else
+                // Still had the item
+                times[item] += CROSS_PLOT_WITH_ITEM;
 
         foundMonster = 0;
     }
 
-    private void crossPotionField() {
-        noItemTime = getMinOfTimes() + 1 + foundMonster;
-        potionTime = noItemTime + 1;
-
-        harpTime += CROSS_WITH_ITEM_TIME;
-        cloakTime += CROSS_WITH_ITEM_TIME;
-
-        foundMonster = 0;
-    }
-
-    private void crossCloakField() {
-        noItemTime = getMinOfTimes() + 1 + foundMonster;
-        cloakTime = noItemTime + 1;
-
-        harpTime += CROSS_WITH_ITEM_TIME;
-        potionTime += CROSS_WITH_ITEM_TIME;
-
-        foundMonster = 0;
-    }
-
-    private void crossDogField() {
-        noItemTime = INF;
-
-        harpTime += HARP_TIME;
-        potionTime += POTION_TIME;
-        cloakTime += CLOAK_TIME;
-
-        foundMonster = 1;
-    }
-
-    private void crossTrollField() {
-        harpTime = noItemTime = INF;
-
-        potionTime += POTION_TIME;
-        cloakTime += CLOAK_TIME;
-
-        foundMonster = 1;
-    }
-
-    private void crossDragonField() {
-        potionTime = harpTime = noItemTime = INF;
-
-        cloakTime += CLOAK_TIME;
+    private void crossPlot(int monster) {
+        for (int item : items)
+            if (item >= monster)
+                times[item] += CROSS_PLOT_WITH_ITEM + item;
+            else
+                times[item] = Integer.MIN_VALUE;
 
         foundMonster = 1;
     }
 
     private int getMinOfTimes() {
-        return Math.min(Math.min(noItemTime, harpTime), Math.min(potionTime, cloakTime));
+        int min = Integer.MAX_VALUE;
+
+        for (int t : times)
+            if (t > 0)
+                min = Math.min(min, t);
+
+        return min;
     }
 }
