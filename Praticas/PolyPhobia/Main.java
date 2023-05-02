932092b4cccd5e0f09134925278234f161c320e5
PedroMMRAF <pmmr.fernandes@campus.fct.unl.pt>
2023-05-02 10:32:44 +0100

    restructure

--- Praticas/PolyPhobia/Main.java
+++ Praticas/PolyPhobia/Main.java
@@ -1,17 +1,15 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.BufferedReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+        try (BufferedReader reader = Util.getReader()) {
             int segmentCount = Integer.parseInt(reader.readLine());
 
             Solver solver = new Solver(segmentCount);
 
             while (segmentCount-- > 0) {
-                int[] line = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                int[] line = Util.readIntLine(reader);
 
                 solver.addEdge(line[0], line[1], line[2], line[3]);
             }
