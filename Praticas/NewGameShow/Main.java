e8bae3fdf7f6338bd9fd7297e2966d55b8f45a62
PedroMMRAF <pmmr.fernandes@campus.fct.unl.pt>
2023-05-30 09:34:22 +0100

    bellman ford

---
+++ Praticas/NewGameShow/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String[] line = reader.readLine().split(" ");
+            int R = Integer.parseInt(line[0]);
+            int D = Integer.parseInt(line[1]);
+
+            Solver solver = new Solver(R, D);
+
+            for (int d = 0; d < D; d++) {
+                line = reader.readLine().split(" ");
+                int r1 = Integer.parseInt(line[0]);
+                int r2 = Integer.parseInt(line[1]);
+                int v = Integer.parseInt(line[2]);
+
+                solver.addEdge(r1, r2, v);
+            }
+
+            line = reader.readLine().split(" ");
+            int S = Integer.parseInt(line[0]);
+            int E = Integer.parseInt(line[1]);
+            int I = Integer.parseInt(line[2]);
+
+            int solution = solver.solve(S, E, I);
+        }
+    }
+}
---
+++ Praticas/NewGameShow/Solver.java
@@ -0,0 +1,17 @@
+public class Solver {
+    public Solver(int nodes, int edges) {
+
+    }
+
+    public void addEdge(int start, int end, int label) {
+
+    }
+
+    public int solve(int start, int goal, int budget) {
+        return -1;
+    }
+
+    private int bellmanFord() {
+        return -1;
+    }
+}
